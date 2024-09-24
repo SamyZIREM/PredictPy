27530cd0598d856b037bdfddd1314dd3317b7ae7
SamyZIREM <91314042+SamyZIREM@users.noreply.github.com>
2024-09-24 17:07:45 +0200

    Séparation des caractéristiques et la cible

--- main.py
+++ main.py
@@ -13,23 +13,27 @@ données_manquantes = df.isnull().sum()
 #Affichage graphique pour voir la répartition entre ceux qui ont remboursé et ceux qui ne l'ont pas fait
 #On conclut que le nbr de personnes ayant remboursé dépasse largement ceux qui n'ont pas remboursé
 
-# plt.figure(figsize=(8, 6))
-# sns.countplot(x='not.fully.paid', data=df, palette="Set2")
-# plt.title('Distribution des emprunteurs ayant remboursé ou non')
-# plt.xlabel('Not Fully Paid (1 = Non remboursé, 0 = Remboursé)')
-# plt.ylabel('Nombre d\'emprunteurs')
-# plt.show()
+plt.figure(figsize=(8, 6))
+sns.countplot(x='not.fully.paid', data=df, palette="Set2")
+plt.title('Distribution des emprunteurs ayant remboursé ou non')
+plt.xlabel('Not Fully Paid (1 = Non remboursé, 0 = Remboursé)')
+plt.ylabel('Nombre d\'emprunteurs')
+#plt.show()
 
 #Conversion des variables catégorielles en numérique (dans notre cas purpose : représente l'objet du prêt)
 encoder = OrdinalEncoder(cols=['purpose'])
 loan_data_encoded = encoder.fit_transform(df)
 #print(loan_data_encoded.head())
 
+#Relation entre le taux d'interet et remboursement 
 plt.figure(figsize=(8, 6))
 sns.boxplot(x='not.fully.paid', y='int.rate', data=loan_data_encoded, palette="Set2")
 plt.title('Relation entre le taux dintérêt et le remboursement')
 plt.xlabel('Not Fully Paid (1 = Non remboursé, 0 = Remboursé)')
 plt.ylabel('Taux dintérêt')
-plt.show()
+#plt.show()
 
 
+#Séparation des caractéristiques et la cible (remboursé ou non)
+X = loan_data_encoded.drop('not.fully.paid', axis=1)  # Caractéristiques
+y = loan_data_encoded['not.fully.paid']  # Cible
