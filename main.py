1da177e7d6e39cb3b34d7dba044a8b0f3fc7d8d6
SamyZIREM <91314042+SamyZIREM@users.noreply.github.com>
2024-09-24 16:24:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt  # Import Matplotlib's pyplot
+import matplotlib.pyplot as plt
 
-# Chargement des données
+#Chargement des données
 df = pd.read_csv('loan_data.csv')
 
-# Vérifier les données manquantes (aucune variable nulle)
+#Vérifier les données manquantes (aucune variable nulle)
 données_manquantes = df.isnull().sum()
-#print(données_manquantes)
+#print(données_manquantes) -> on n'a pas de valeurs manquantes 
 
-# Affichage graphique pour voir la répartition entre ceux qui ont remboursé et ceux qui ne l'ont pas fait
+#Affichage graphique pour voir la répartition entre ceux qui ont remboursé et ceux qui ne l'ont pas fait
+#On conclut que le nbr de personnes ayant remboursé dépasse largement ceux qui n'ont pas remboursé
 plt.figure(figsize=(8, 6))
 sns.countplot(x='not.fully.paid', data=df, palette="Set2")
 plt.title('Distribution des emprunteurs ayant remboursé ou non')
 plt.xlabel('Not Fully Paid (1 = Non remboursé, 0 = Remboursé)')
 plt.ylabel('Nombre d\'emprunteurs')
 plt.show()
+
+
 
