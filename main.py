745c928ea8de8ad65d9db6c786d00ddbbfccd5c0
SamyZIREM <91314042+SamyZIREM@users.noreply.github.com>
2024-09-30 10:14:43 +0200

    Ajout du modèle de Régression Logistique et évaluation

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from category_encoders import OrdinalEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+
 
 #Chargement des données
 df = pd.read_csv('loan_data.csv')
@@ -15,7 +20,7 @@ données_manquantes = df.isnull().sum()
 #On conclut que le nbr de personnes ayant remboursé dépasse largement ceux qui n'ont pas remboursé
 
 plt.figure(figsize=(8, 6))
-sns.countplot(x='not.fully.paid', data=df, palette="Set2")
+sns.countplot(x='not.fully.paid', data=df, hue='not.fully.paid', legend=False, palette="Set2")
 plt.title('Distribution des emprunteurs ayant remboursé ou non')
 plt.xlabel('Not Fully Paid (1 = Non remboursé, 0 = Remboursé)')
 plt.ylabel('Nombre d\'emprunteurs')
@@ -28,7 +33,7 @@ loan_data_encoded = encoder.fit_transform(df)
 
 #Relation entre le taux d'interet et remboursement 
 plt.figure(figsize=(8, 6))
-sns.boxplot(x='not.fully.paid', y='int.rate', data=loan_data_encoded, palette="Set2")
+sns.boxplot(x='not.fully.paid', y='int.rate', hue='not.fully.paid', data=loan_data_encoded, legend=False, palette="Set2")
 plt.title('Relation entre le taux dintérêt et le remboursement')
 plt.xlabel('Not Fully Paid (1 = Non remboursé, 0 = Remboursé)')
 plt.ylabel('Taux dintérêt')
@@ -41,3 +46,17 @@ y = loan_data_encoded['not.fully.paid']  # Cible
 
 #Division des données en set train et set test (80% entraînement et 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Normalisation des données
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Modèle de Régression Logistique
+model = LogisticRegression(max_iter=5000, solver='liblinear')
+model.fit(X_train_scaled, y_train)
+
+# Évaluation du modèle
+y_pred = model.predict(X_test_scaled)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy of the model: {accuracy * 100:.2f}%')
